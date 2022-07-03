bc6f22ee2f01459c713332baedf3b11f0e6d2672
Oscar Reparaz <oreparaz@users.noreply.github.com>
2022-07-02 20:36:28 -0700

    feat: add cbmc proof
    cbmc is an easy to use bounds checker. This commit checks for memory safety
    (array bounds checks and checks for the safe use of pointers) and adds a
    github action to test this in CI

---
+++ verify/vrt_cbmc_harness.c
@@ -0,0 +1,31 @@
+#include "../vrt.h"
+
+#define CHECK(x)                                                               \
+  do {                                                                         \
+    int ret;                                                                   \
+    if ((ret = x) != VRT_SUCCESS) {                                            \
+      return (ret);                                                            \
+    }                                                                          \
+  } while (0)
+
+#define CHECK_TRUE(x, errorcode)                                               \
+  do {                                                                         \
+    if (!(x))                                                                  \
+      return (errorcode);                                                      \
+  } while (0)
+
+#define CHECK_NOT_NULL(x) CHECK_TRUE((x) != NULL, VRT_ERROR_NULL_ARGUMENT)
+
+#define RECV_BUFFER_LEN 1024
+
+void vrt_cbmc_harness(void) {
+	uint8_t nonce_sent[VRT_NONCE_SIZE];
+	uint8_t reply[RECV_BUFFER_LEN];
+	uint8_t pk[32];
+	uint64_t *out_midp;
+	uint32_t *out_radii;
+
+	CHECK(vrt_parse_response(nonce_sent, sizeof(nonce_sent),
+                              reply, sizeof(reply), pk,
+                              out_midp, out_radii));
+}
