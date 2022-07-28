34e4d313b64802f39fdf26d78ce3ddf6eb47ac59
Oscar Reparaz <oreparaz@users.noreply.github.com>
2022-07-28 11:46:20 +0200

    ci: add fuzzers to ci via ClusterFuzzLite
    We plug fuzzing tests to GitHub Actions via ClusterFuzzLite.
    This has the advantage that docker images for fuzzing are
    managed for us.
    
    Followed https://google.github.io/clusterfuzzlite/running-clusterfuzzlite/github-actions/

--- examples/compile.sh
+++ examples/compile.sh
@@ -6,7 +6,7 @@ CC=clang
 
 rm -f *.o client
 $CC -c ../tweetnacl.c -o tweetnacl.o
-$CC -Wall -Werror -g -fsanitize=address,undefined -c ../vrt.c -o vrt.o 
+$CC -Wall -Werror -g -fsanitize=address,undefined -c ../vrt.c -o vrt.o
 
 $CC -I ../ -Wall -Werror -g -fsanitize=address,undefined -o client vrt_client_unix.c vrt.o tweetnacl.o && ./client
 
